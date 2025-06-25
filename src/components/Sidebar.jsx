4178d61cbed7e934a66b74f09ea4503360f70b8d
Sahilsoni09 <Sahilsoni7609@gmail.com>
2025-06-25 19:50:55 +0530

    implemented sidebar app navigation

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,11 +1,14 @@
+import { Outlet } from "react-router-dom";
 import styles from "./Sidebar.module.css";
+import Logo from "./Logo";
+import AppNav from "./AppNav";
 
 function Sidebar() {
   return (
     <div className={styles.sidebar}>
       <Logo />
       <AppNav />
-      <p> List of Companies</p>
+      <Outlet />
       <footer className={styles.footer}>
         <p className={styles.copyright}>
           &copy; {new Date().getFullYear()} Your Company Name. All rights
